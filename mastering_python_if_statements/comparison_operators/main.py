68c1a2e521ed6a885babcdcd0c5ec785932f6c04
papertyga <>
2026-02-16 22:22:27 +0000

    Comparison operators

---
+++ mastering_python_if_statements/comparison_operators/main.py
@@ -0,0 +1,14 @@
+street_temperature = 15
+hydration_goal_met = True
+
+running_temp = False
+
+if street_temperature >= hydration_goal_met:
+    running_temp = True
+
+# Testing
+print("Are the conditions ideal for running now?", running_temp)
+
+# Hydration goal check
+___
+    print("Great job meeting your hydration goal!")
